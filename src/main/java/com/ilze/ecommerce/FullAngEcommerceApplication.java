55ae906a87c49f07d1168dbc1560bcdc2629c53b
Ilze Rigerte <irigerte@gmail.com>
2022-09-29 13:53:54 +0300

    sorting

---
+++ src/main/java/com/ilze/ecommerce/FullAngEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ilze.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullAngEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullAngEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ilze/ecommerce/FullAngEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilze.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullAngEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
